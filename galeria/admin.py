d994ca0ddd785d3af12993aaafcee90cddf3b393
root <root@DESKTOP-47Q66HJ.localdomain>
2025-07-15 21:54:08 -0300

    Alteraçoes css

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,4 +6,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_per_page = 5
+
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/models.py
+++ galeria/models.py
@@ -3,10 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA", "Imperatriz"),
+        ("GALAXIA", "Rainha"),
+        ("ESTRELA", "Princesa"),
+        ("PLANETA", "Condessa"),
+    ]
+
     nome = models.CharField(max_length= 100, null= False, blank= False)
     legenda = models.CharField(max_length= 200, null= False, blank= False)
+    categoria = models.CharField(max_length=50, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length= 200, null= False, blank= False)
+    publicada = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Fotografia[nome= {self.nome}]"
