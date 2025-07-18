f726657ff55a8dd9be2b129b54c6a5db2b305293
root <root@DESKTOP-47Q66HJ.localdomain>
2025-07-18 10:54:28 -0300

    finalizaçao do curso de django mexendo com banco de dados bora de next

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,10 +3,11 @@ from galeria.models import Fotografia
 # Register your models here.
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
     list_filter = ("categoria",)
+    list_editable =("publicada",)
     list_per_page = 5
 
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/views.py
+++ galeria/views.py
@@ -7,9 +7,19 @@ from galeria.models import Fotografia
 
 
 def index (request):
-   fotografia = Fotografia.objects.all()
+   fotografia = Fotografia.objects.filter(publicada=True) #order_by("data_fotografia") caso eu queira ordenar pela data
    return render(request, 'galeria/index.html', {"cards": fotografia})
 
 def imagem (request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk= foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar (request):
+
+    fotografia = Fotografia.objects.filter(publicada=True)
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = Fotografia.objects.filter(nome__icontains= nome_a_buscar, publicada= True)
+
+    return render(request, "galeria/buscar.html", {"cards": fotografias})
